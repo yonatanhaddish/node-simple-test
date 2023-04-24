8da232c42bcb73dea734827866c14fd5a092aaf6
Yonatan Haddish <yonihailshn@gmail.com>
2023-04-24 18:15:44 -0400

    simple super hero story about two friends

---
+++ app.js
@@ -0,0 +1,44 @@
+function getRandomStringFromArray(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+// Example usage
+const myArray = [
+  "Adamu, Ab",
+  "Chinedu",
+  "Anisa",
+  "Anne",
+  "Abdulahi",
+  "Sam",
+  "Sergio",
+  "Iyinoluwa",
+  "Dave",
+  "Moss",
+];
+const randomString = getRandomStringFromArray(myArray);
+console.log(
+  "------------------------------------------------------------------------------"
+);
+console.log(`The chosen name is "${randomString}"`);
+console.log(
+  "------------------------------------------------------------------------------"
+);
+
+/* Chinedu and Sergio were two ordinary guys living in the city. But one day, they were caught in the middle of a dangerous experiment gone wrong.
+
+As the experiment exploded, they were both hit by a blast of energy and knocked unconscious. When they woke up, they realized they had gained incredible superpowers.
+
+Chinedu now had the power to manipulate and control the elements - he could summon lightning bolts, create whirlwinds, and control the flow of water.
+
+Sergio, on the other hand, had the power of super strength and durability. He could lift cars with ease and withstand any physical attack.
+
+Together, they became the unstoppable duo - using their powers to protect their city and fight against evil. They were known as Thunderbolt and Titan.
+
+One day, a group of supervillains attacked the city with their advanced technology, causing destruction and chaos. Thunderbolt and Titan didn't hesitate to jump into action.
+
+Chinedu summoned a powerful thunderstorm to distract the villains, while Sergio charged straight into the fray, taking out the bad guys with his incredible strength.
+
+With their combined efforts, they defeated the villains and saved the city from destruction. The people cheered and hailed them as heroes.
+
+From that day on, Thunderbolt and Titan continued to use their powers for good, protecting their city from any threats that came their way. */
